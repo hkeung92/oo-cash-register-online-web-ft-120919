d4e3e233bda7f882f532334771e1126991df2767
Ho Yin Keung <hykeung92@gmail.com>
2019-12-30 02:41:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,8 +10,8 @@ class CashRegister
 
   def add_item(title, amount, quantity=1)
     self.total += amount * quantity
-    quantity.times do
-      items << title
+    # quantity.times do
+    #   items << title
     end
     self.last_transaction = amount * quantity
   end
